1afb24fdf62e04cc13effd8b84a46b36dca10e21
udit01 <uditjainiitd@gmail.com>
2017-08-15 02:02:32 +0530

    cleanedUp for linked list implementation

--- SinglyLinkedListImplementation/CheckLL.java
+++ SinglyLinkedListImplementation/CheckLL.java
@@ -2,6 +2,49 @@ public class CheckLL {
     public static void main(String args[]){
     	System.out.println("Linked List checker class:");
         
+        ListNode<Integer> ln = new ListNode<Integer>(1);
+        ListNode<Integer> ln2 = new ListNode<Integer>(2,ln);
+        ListNode<Integer> ln3 = new ListNode<Integer>(3,ln);
+        ListNode<Integer> ln5 = new ListNode<Integer>(5,ln3);
+        
+		//try {
+            System.out.println("Element for ln is:" + ln.getElement());
+            System.out.println("Element for ln2 is:" + ln2.getElement());
+            System.out.println("Element for ln3 is:" + ln3.getElement());
+            System.out.println("Element for ln5 is:" + ln5.getElement());
+        //} catch (CustomException e) {
+        //    e.printStackTrace();
+        //}
+        
+        try {//is this allowed? Logically yes as at a time only one exception can propogate
+
+            System.out.println("Next node elem for ln2 is:" + ln2.getNext().getElement());
+            System.out.println("Next node elem for ln3 is:" + ln3.getNext().getElement());
+            System.out.println("Next node elem for ln5 is:" + ln5.getNext().getElement());
+            //THE BELOW STATEMENT SHOULD DEFINATELY THROW AN EXCEPTION
+            System.out.println("Next node elem for ln is:" + ln.getNext().getElement());
+        } catch (CustomException e) {
+            e.printStackTrace();
+        }
+        
+        ln2.setNext(ln3);
+        ln.setNext(ln2);
+        ln3.setNext(ln);
+        
+        try {//is this allowed? Logically yes as at a time only one exception can propogate
+
+			System.out.println("BELOW LINE SHOULD PRINT 3");
+            System.out.println("Next node elem for ln2 is:" + ln2.getNext().getElement());
+			System.out.println("BELOW LINE SHOULD PRINT 1");
+            System.out.println("Next node elem for ln3 is:" + ln3.getNext().getElement());
+			System.out.println("BELOW LINE SHOULD PRINT 3");
+            System.out.println("Next node elem for ln5 is:" + ln5.getNext().getElement());
+			System.out.println("BELOW LINE SHOULD PRINT 2");
+            System.out.println("Next node elem for ln is:" + ln.getNext().getElement());
+        } catch (CustomException e) {
+            e.printStackTrace();
+        }
+        
         System.out.println("END");
         
     }
--- SinglyLinkedListImplementation/LinkedListInterface.java
+++ SinglyLinkedListImplementation/LinkedListInterface.java
@@ -1,14 +1,5 @@
 import java.lang.*;
 
-public interface StackInterface<E>{
-    //private int MAX_ELEMENTS;
-    //private int numElements;
-    //private E[] a;
-    //public stack();
-    //public stack(int n);
-    public E top() throws CustomException;
-    public E pop() throws CustomException;
-    public void push(E e) throws CustomException;
-    public int size() ;
-    public void printStack() throws CustomException;
+public interface LinkedListInterface<E>{
+	
 }
