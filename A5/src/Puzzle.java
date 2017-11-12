59f87189323ea6bde7b08923d523051a1034369c
udit01 <uditjainiitd@gmail.com>
2017-11-12 15:15:18 +0530

    worte it but to write the functions

--- A5/src/Puzzle.java
+++ A5/src/Puzzle.java
@@ -1,10 +1,14 @@
 import javafx.util.Pair;
 
 import java.io.*;
+import java.lang.reflect.Array;
 import java.util.*;
 
 public class Puzzle {
     static HashMap<String,ArrayList<String>> graph;
+    static HashMap<String,ArrayList<String>> graphM;
+    static ArrayList<Pair<Integer,String>> heap;
+
 //    private Pair<Integer[][], Pair<Integer, Integer>> ;
 
 //    HashMap<Node,ArrayList<Node>> gr;
@@ -21,13 +25,13 @@ public class Puzzle {
         String[] splice;
         String[] w;
         BufferedReader readerI;//input reader
+//        Integer[] moveAndCost;
 //        BufferedWriter writerO;
         try {
-//            startTime = System.currentTimeMillis();
+            startTime = System.currentTimeMillis();
             readerI = new BufferedReader(new InputStreamReader(new FileInputStream(args[0])) );
             oStream = new BufferedOutputStream(new FileOutputStream(args[1]));
 
-            time1 = System.currentTimeMillis();
 //-------------test code
 //            ArrayList<String> dummy = new ArrayList<String>();
 //            dummy.add("random");
@@ -36,26 +40,34 @@ public class Puzzle {
 //            graph.put("12345678G",dummy);
 //            printGraph();
 //
+
             graph = new HashMap<String, ArrayList<String>>();
-            constructGraph();
-            printGraph();
-//            t = Integer.parseInt(readerI.readLine());
+            graphM = new HashMap<String, ArrayList<String>>();
+
+            constructGraphs();
+            time1 = System.currentTimeMillis();
+//            printGraphs();
+            t = Integer.parseInt(readerI.readLine());
 //
 //
-//            for(int i=0;i<t;i++){
-//                splice = readerI.readLine().split(" ");
-//                //splice[0] contatins the start and other end
-//                w = readerI.readLine().split(" ");
-//                for (int k=0;k<8;k++){
-//                    weights[k]=Integer.parseInt(w[k]);
-//                }
-////                constructGraph();//once per the t test cases or
-//
-//            }
+            for(int i=0;i<t;i++){
+                splice = readerI.readLine().split(" ");
+                //splice[0] contatins the start and other end
+                w = readerI.readLine().split(" ");
+                for (int k=0;k<8;k++){
+                    weights[k]=Integer.parseInt(w[k]) + 1;//remember as we don't want 0 paths to trouble us and we will subtract after
+                    //these are actually pseudo weights
+                }
+
+//                moveAndCost =  dijkstra(splice[0],splice[1]);//will also print the moves and cost!yaya
+                dijkstra(splice[0],splice[1]);//will also print the moves and cost!yaya
+
+            }
 
 
             time2 = System.currentTimeMillis();
-            oStream.write(("(Partial?)Graph formed in: "+(time2-time1)+"ms\n").getBytes());
+            oStream.write(("Graph(s) formed in: "+(time1-startTime)+"ms\n").getBytes());
+            oStream.write(("Input Processed in: "+(time2-time1)+"ms\n").getBytes());
             oStream.flush();
             oStream.close();
 
@@ -66,14 +78,95 @@ public class Puzzle {
         }
     }
 
-    public static void constructGraph(){//will make changes in the global graph variable
+    public static void dijkstra(String start,String end)throws IOException{
+        //IO -- Prints the total moves and cost requried to traverse from start to end! also print the moves
+        //if in different components then immediatly exit
+        int c=0;
+//        for (HashMap<String,ArrayList<String>>g : {graph,graphM})
+        if (graph.containsKey(start)){
+            c=1;
+            if (!graph.containsKey(end)){
+                //print something and then return nul
+                oStream.write("-1 -1\n\n".getBytes());
+                return;
+            }
+        }
+        else if (graphM.containsKey(start)){
+            c=2;
+            if (!graphM.containsKey(end)){
+                //print something and then return nul
+                oStream.write("-1 -1\n\n".getBytes());
+                return;
+            }
+        }
+        //hash map for parent pointers and hash map for storing indices in the array
+
+        HashMap<String,ArrayList<String>>g = (c==1)?graph:graphM;
+        HashMap<String,Integer> indiceMap = new HashMap<String, Integer>();
+        HashMap<String,Integer> parentIndice = new HashMap<String,Integer>();
+        //
+        heap = new ArrayList<Pair<Integer, String>>(181440);
+
+        ArrayList<String> tempNeighbours = g.get(start);//optimization
+        g.remove(start);//OPTIMIZATION?remove first then
+
+        int l = 0;
+        heap.add(new Pair<Integer, String>(0,start));//added it at the 0th postion
+        indiceMap.put(start,0);
+        parentIndice.put(start,null);//start has no parent
+        ++l;
+        for (String k : g.keySet() ){//initialization
+            //no need to check as we are adding 0th initially
+            heap.add(new Pair<Integer, String>(Integer.MAX_VALUE,k));//yay//unmarked nodes
+            indiceMap.put(k,l);
+            parentIndice.put(k,null);//null integer
+            //now all nodes are in heap
+            ++l;
+        }
+
+        g.put(start,tempNeighbours);//reinserting after
+        //constructed the outside of the cloud
+        //some kind of parent pointer
+        int w = 0;
+        Pair<Integer,String> current;
+        String node;
+        int nodeDist=0;
+        int neighbourDist=0;
+        while (!heap.isEmpty()){
+            //
+             current = deleteMin();//it will throw out what ?
+            node = current.getValue();
+            //current is int , string ie distance and node
+
+            //if current is end then CHANDIII
+
+            if(node.equals(end)){
+                //yippie! do something then break!
+            }
+
+            nodeDist = heap.get(indiceMap.get(node)).getKey();
+            for (String neighbour:g.get(node)){//getting the neighbours of node from graphs
+                //dist of neighbour = dist of current + calculate cost(s,current)
+                w = calculateCost(node,neighbour);
+                neighbourDist = heap.get(indiceMap.get(neighbour)).getKey();
+                if (neighbourDist>nodeDist+w){
+                    heap.set(indiceMap.get(neighbour),new Pair<>(nodeDist+w,neighbour));
+                    percolateUp(indiceMap.get(neighbour));//this value has decreased so required to be maintained
+                    //then percolate up or somthing ?
+                    parentIndice.put(neighbour,indiceMap.get(node));
+                }
+            }
+
+        }
+        //what do we need to do?
+    }
+
+    public static void constructGraphs(){//will make changes in the global graph variable
         String s = "12345678G";
-        String sM = "321654G87";
         Queue<String> q = new ArrayDeque<String>();//can be faster?
-//        q.add(s);
-        q.add(sM);
+        q.add(s);
         String current;
-        ArrayList<String> nbors;
+        ArrayList<String> nbors = new ArrayList<String>();
 
         while(!q.isEmpty()){//this can be made much faster by marking
             current = q.remove();
@@ -85,21 +178,50 @@ public class Puzzle {
 
             q.addAll(nbors);
         }
+
+        //making the graph gM
+        String sM = "321654G87";
+
+        Queue<String> qM = new ArrayDeque<String>();//can be faster?
+        qM.add(sM);
+        nbors.clear();
+        while(!qM.isEmpty()){//this can be made much faster by marking
+            current = qM.remove();
+            if (graphM.containsKey(current)){
+                continue;
+            }
+            nbors = generateNeigbours(current);//can be made faster
+            graphM.put(current,nbors);
+
+            qM.addAll(nbors);
+        }
     }
-    public static void printGraph(){
+    public static void printGraphs(){
 //        Set<String> kSet =  graph.keySet();
         ArrayList<String> l;
         try {
+            oStream.write(("Graph 1:\n").getBytes());
             for (String key : graph.keySet()) {
                 l = graph.get(key);
-                oStream.write(("String:" + key + " Neighbours:{").getBytes());
+                oStream.write(("Node:" + key + " Neighbours{").getBytes());
                 for (String nbr:l){
-                    oStream.write((nbr+", ").getBytes());
+                    oStream.write((nbr+" ").getBytes());
                 }
                 oStream.write((" }\n").getBytes());
 
             }
             oStream.flush();
+            //printing the 2nd graphs
+            oStream.write(("Graph 2:\n").getBytes());
+            for (String key : graphM.keySet()) {
+                l = graphM.get(key);
+                oStream.write(("Node:" + key + " Neighbours{").getBytes());
+                for (String nbr:l){
+                    oStream.write((nbr+" ").getBytes());
+                }
+                oStream.write((" }\n").getBytes());
+
+            }
         }
         catch (Exception e){
             e.printStackTrace();
@@ -308,7 +430,20 @@ public class Puzzle {
 //    }
 
 }
-//class Node{
-//    String s;
-////    ArrayList<Node> neighbours;
-//}
+class Node implements Comparable<Node> {
+    //what does it have ?
+    String node = null;
+    String parent = null;
+    //a marked boolean ?
+    Integer distance = Integer.MAX_VALUE;//by default until we override it ?
+
+//    Node(){
+//        //somthing?
+//    }
+
+    @Override
+    public int compareTo(Node n){//what will be the order of dist ?
+        return this.distance.compareTo(n.distance);
+    }
+    //anything more ?
+}
